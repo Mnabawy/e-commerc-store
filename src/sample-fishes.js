58a0c0140d2c816de6a80a8c73bb7ba5d861ffcb
Mnabawy <hassanm486@yahoo.com>
2019-03-25 19:14:34 +0200

    first

---
+++ src/sample-fishes.js
@@ -0,0 +1,76 @@
+// This is just some sample data so you don't have to think of your own!
+const fishes = {
+  fish1: {
+    name: 'Pacific Halibut',
+    image: 'https://image.shutterstock.com/image-photo/grilled-fish-260nw-580964716.jpg',
+    desc: 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price: 1724,
+    status: 'available'
+  },
+
+  fish2: {
+    name: 'Lobster',
+    image: 'https://image.shutterstock.com/image-photo/grilled-fish-260nw-580964716.jpg',
+    desc: 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
+    price: 3200,
+    status: 'available'
+  },
+
+  fish3: {
+    name: 'Sea Scallops',
+    image: 'https://image.shutterstock.com/z/stock-photo-fish-fillet-with-fried-vegetables-garnish-and-chutney-770048845.jpg',
+    desc: 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
+    price: 1684,
+    status: 'unavailable'
+  },
+
+  fish4: {
+    name: 'Mahi Mahi',
+    image: 'https://image.shutterstock.com/image-photo/plate-grilled-salmon-steak-vegetables-260nw-673973287.jpg',
+    desc: 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
+    price: 1129,
+    status: 'available'
+  },
+
+  fish5: {
+    name: 'King Crab',
+    image: 'https://image.shutterstock.com/image-photo/plate-grilled-salmon-steak-vegetables-260nw-673973287.jpg',
+    desc: 'Crack these open and enjoy them plain or with one of our cocktail sauces',
+    price: 4234,
+    status: 'available'
+  },
+
+  fish6: {
+    name: 'Atlantic Salmon',
+    image: 'https://image.shutterstock.com/image-photo/plate-grilled-salmon-steak-vegetables-260nw-673973287.jpg',
+    desc: 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
+    price: 1453,
+    status: 'available'
+  },
+
+  fish7: {
+    name: 'Oysters',
+    image: 'https://image.shutterstock.com/image-photo/plate-grilled-salmon-steak-vegetables-260nw-673973287.jpg',
+    desc: 'A soft plump oyster with a sweet salty flavor and a clean finish.',
+    price: 2543,
+    status: 'available'
+  },
+
+  fish8: {
+    name: 'Mussels',
+    image: 'https://image.shutterstock.com/image-photo/plate-grilled-salmon-steak-vegetables-260nw-673973287.jpg',
+    desc: 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
+    price: 425,
+    status: 'available'
+  },
+
+  fish9: {
+    name: 'Jumbo Prawns',
+    image: 'https://image.shutterstock.com/image-photo/baked-salmon-fillet-rosemary-lemon-260nw-341238107.jpg',
+    desc: 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
+    price: 2250,
+    status: 'available'
+  }
+};
+
+export default fishes;

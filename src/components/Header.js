2e031063506a4a658389b175bcea4e787740983c
Mnabawy <hassanm486@yahoo.com>
2019-04-06 17:40:39 +0200

    auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,14 @@ import React from 'react';
 const Header = (props) => {
   return (
     <header className="top">
-      <h1>
+      <h3>
         Catch
         <span className="ofThe">
           <span className="of">of</span>
           <span className="the">the</span>
         </span>
         Day
-      </h1>
+      </h3>
       <h3 className="tagline"><span>{props.tagline}</span></h3>
     </header>
   )
